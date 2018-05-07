44da228600cf7d6b521af69e3640b6ed4cfe0540
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-05-08 00:12:28 +0200

    Add geometry validate and clean methods

--- test/geometry/clean.js
+++ test/geometry/clean.js
@@ -2,34 +2,8 @@ const test = require('ava')
 const {deintersectPolygon} = require('../../lib/geometry/clean')
 
 test('deintersectPolygon: regular behavior', t => {
-  const selfIntersectedPolygon = {
-    type: 'Polygon',
-    coordinates: [
-      [
-        [
-          0.06,
-          46.57
-        ],
-        [
-          0.71,
-          47.82
-        ],
-        [
-          2.48,
-          46.05
-        ],
-        [
-          1.93,
-          47.65
-        ],
-        [
-          0.06,
-          46.57
-        ]
-      ]
-    ]
-  }
-  const deintersectedPolygon = deintersectPolygon(selfIntersectedPolygon)
+  const selfIntersectingPolygon = require('./fixtures/self-intersecting-polygon.json')
+  const deintersectedPolygon = deintersectPolygon(selfIntersectingPolygon)
   t.is(deintersectedPolygon.type, 'MultiPolygon')
   t.is(deintersectedPolygon.coordinates.length, 2)
 })
---
+++ test/geometry/validate.js
@@ -0,0 +1,61 @@
+const test = require('ava')
+const {validate} = require('../../lib/geometry/validate')
+
+test('validate: self-intersecting polygon', t => {
+  const poly = require('./fixtures/self-intersecting-polygon.json')
+  const result = validate(poly)
+  t.true(result.includes('has-self-intersection'))
+  t.is(result.length, 1)
+})
+
+test('validate: polygon ring with too few coordinates', t => {
+  const poly = {type: 'Polygon', coordinates: [[[0, 0], [0, 1], [1, 1]]]}
+  const result = validate(poly)
+  t.true(result.includes('ring-too-few-coords'))
+  t.true(result.includes('ring-not-closed'))
+  t.is(result.length, 2)
+})
+
+test('validate: polygon ring not closed', t => {
+  const poly = {type: 'Polygon', coordinates: [[[0, 0], [0, 1], [1, 1], [1, 0]]]}
+  const result = validate(poly)
+  t.true(result.includes('ring-not-closed'))
+  t.is(result.length, 1)
+})
+
+test('validate: polygon ring has duplicate vertices', t => {
+  const poly = {type: 'Polygon', coordinates: [[[0, 0], [0, 1], [1, 1], [1, 0], [0, 1], [1, 1], [0, 0]]]}
+  const result = validate(poly)
+  t.true(result.includes('ring-has-duplicate-vertices'))
+  t.true(result.includes('has-self-intersection'))
+  t.is(result.length, 2)
+})
+
+test('validate: polygon without ring', t => {
+  const poly = {type: 'Polygon', coordinates: []}
+  const result = validate(poly)
+  t.true(result.includes('polygon-without-rings'))
+  t.is(result.length, 1)
+})
+
+test('validate: polygon has exterior holes', t => {
+  const poly = {type: 'Polygon', coordinates: [
+    [[0, 0], [0, 1], [1, 1], [1, 0], [0, 0]],
+    [[4, 4], [4, 5], [5, 5], [5, 4], [4, 4]]
+  ]}
+  const result = validate(poly)
+  t.true(result.includes('has-exterior-holes'))
+  t.is(result.length, 1)
+})
+
+test('validate: polygon has crossing holes', t => {
+  const poly = {type: 'Polygon', coordinates: [
+    [[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]],
+    [[4, 4], [4, 6], [6, 6], [6, 4], [4, 4]],
+    [[5, 5], [5, 7], [7, 7], [7, 5], [5, 5]]
+  ]}
+  const result = validate(poly)
+  t.true(result.includes('has-crossing-holes'))
+  t.true(result.includes('has-self-intersection'))
+  t.is(result.length, 2)
+})
