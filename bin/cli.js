b38ad35d6827e01abdf3825d1609a6e17a2a0dce
Thomas Gratier <thomas_gratier@yahoo.fr>
2025-06-30 10:30:39 +0200

    Update to support no reprojection as an option

--- bin/cli.js
+++ bin/cli.js
@@ -2,13 +2,14 @@
 import process from 'node:process'
 import getStdin from 'get-stdin'
 import debugFactory from 'debug'
-import {parse} from '../lib/index.js'
+import {parse, parseSrs} from '../lib/index.js'
 
 const debug = debugFactory('edigeo-parser')
 
 async function doStuff() {
   const buf = await getStdin.buffer()
-  const {layers} = await parse(buf)
+  const srs = await parseSrs(buf)
+  const {layers} = await parse(buf, {targetSrs: srs})
   console.log('Found %d layers!', Object.keys(layers).length)
   for (const [key, value] of Object.entries(layers)) {
     console.log(`Layer name ${key}: ${value.length}`)
@@ -16,6 +17,8 @@ async function doStuff() {
       debug(JSON.stringify(feature))
     }
   }
+
+  console.log(srs)
 }
 
 doStuff().catch(error => {
